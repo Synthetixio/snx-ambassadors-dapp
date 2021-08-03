708d7c6aa8496bc8df806b880b997797f2497911
Roelani-i3 <roelani@i3.vc>
2021-08-03 16:35:34 +0200

    Revert "Update index.tsx"
    This reverts commit bafde50bff6640e4f696c30bb7d1772b9610f512.

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -16,6 +16,7 @@ return (
     <Head>
       <title>Blog | Synthetix Ambassadors</title>
       <link rel="icon" href="/favicon.ico" />
+      
       ;<link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
         rel="stylesheet"
