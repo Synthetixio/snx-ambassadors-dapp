02cfd62826eae3317df62bb36ceae8fc4067affa
Roelani-i3 <roelani@i3.vc>
2021-08-03 16:56:20 +0200

    reset
    basic updates

--- constants/routes.ts
+++ constants/routes.ts
@@ -1,6 +1,8 @@
 export const ROUTES = {
 	Vote: 'https://staking.synthetix.io/gov/snxambassador.eth',
 	Protocol: (ticker: string) => `/protocol/${ticker}`,
+	Delegate: '/delegate',
+	Blog: '/blog',
 };
 
 export default ROUTES;
